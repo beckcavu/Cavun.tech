8c2f252c744855ba173c422bb55d0c4c9e7786e3
Cavun Beck <beckcavu@umich.edu>
2020-10-21 23:45:43 -0400

    patch page and some dark mode work

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ function changeButtonHTML(obj)
 {
     if (obj.innerHTML == "Dark Mode")
     {
+        document.getElementById("body").style.backgroundColor = "white";
+        document.getElementById("p2").style.color = "black";
         obj.innerHTML = "Light Mode";
         return
     }
-    else{
+    else if (obj.innerHTML == "Light Mode"){
+        document.getElementById("p2").style.color = "white";
+        document.getElementById("body").style.backgroundColor = "#121212";
         obj.innerHTML = "Dark Mode";
         return;
     }
