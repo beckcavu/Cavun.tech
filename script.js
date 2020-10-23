718c5d17ab935a52d02e83f8bd7231967e4ac157
Cavun Beck <beckcavu@umich.edu>
2020-10-23 11:01:06 -0400

    background and resume headers

--- script.js
+++ script.js
@@ -2,14 +2,16 @@ function changeButtonHTML(obj)
 {
     if (obj.innerHTML == "Dark Mode")
     {
-        document.getElementById("body").style.backgroundColor = "white";
+        document.body.style.backgroundColor = "white";
         document.getElementById("p2").style.color = "black";
+        document.body.style.backgroundImage = "url('NYCDAY.png')";
         obj.innerHTML = "Light Mode";
         return
     }
-    else if (obj.innerHTML == "Light Mode"){
+    if (obj.innerHTML == "Light Mode"){
         document.getElementById("p2").style.color = "white";
         document.getElementById("body").style.backgroundColor = "#121212";
+        document.body.style.backgroundImage = "url('NYCNIGHT.png')";
         obj.innerHTML = "Dark Mode";
         return;
     }
